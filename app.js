8534975350d7b3f0711118795f00c843c4a7cb49
dougachrist <dougachrist@gmail.com>
2016-06-14 15:27:09 -0700

    added question 2

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ var userName = prompt('Greetings!!! What is your name?');
 alert('Welcome ' + userName + ' glad you\'re here!');
 
 var answer1 = prompt('does sam have 3 cats?').toLowerCase();
-
 if (answer1 === 'yes') {
   alert('Right! he has 3 cats named Bob, Ryan, and Matt');
 }
 else{
   alert('WRONG - he actually has 3 cats');
 }
+
+var answer2 = promt('Have I been to New York City?')
+if(answer2 === 'yes') {
+  alert('Correct! I visted New York City once!')
+}
+else{
+  alert('Wrong! - I actually have been to New York City')
+}
+
+var answer3 = promt('')
