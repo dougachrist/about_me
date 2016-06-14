866b1df02183338fb345af14d35418ce218528c3
dougachrist <dougachrist@gmail.com>
2016-06-14 14:54:27 -0700

    first question finish

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 'use strict';
 
-alert('wooow');
+var userName = prompt('Greetings!!! What is your name?');
+alert('Welcome ' + userName + ' glad you\'re here!');
+
+var answer1 = prompt('does sam have 3 cats?').toLowerCase();
+
+if (answer1 === 'yes') {
+  alert('Right! he has 3 cats named Bob, Ryan, and Matt');
+}
+else{
+  alert('WRONG - he actually has 3 cats');
+}
