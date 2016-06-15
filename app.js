da25949ebdbd30518be5487b513f7f8765feae66
dougachrist <dougachrist@gmail.com>
2016-06-15 14:13:44 -0700

    added Number guessing game to js

--- app.js
+++ app.js
@@ -4,47 +4,89 @@
 function startGame() {
 
 // Greeting the user
-  var userName = prompt('Greetings!!! What is your name?');
-  alert('Welcome ' + userName + ', glad you\'re here! \nI have a short quiz about me for you to complete.');
+//  var userName = prompt('Greetings!!! What is your name?');
+//  alert('Welcome ' + userName + ', glad you\'re here! \nI have a short quiz about me for you to complete.');
 
-//Questestions 1 - 5
-  var answer1 = prompt('Do I have a dog?').toLowerCase();
-  if(answer1 === 'yes' || answer1 === 'y') {
-    alert('Right! Her name is Lucy.');
-  }
-  else{
-    alert('WRONG - I actually do have a dog.');
-  }
+// //Questestions 1 - 5
+//   var answer1 = prompt('Do I have a dog?').toLowerCase();
+//   if(answer1 === 'yes' || answer1 === 'y') {
+//     alert('Right! Her name is Lucy.');
+//   }
+//   else{
+//     alert('WRONG - I actually do have a dog.');
+//   }
+//
+//   var answer2 = prompt('Have I been to New York City?').toLowerCase();
+//   if(answer2 === 'yes' || answer2 === 'y') {
+//     alert('Correct! I visted New York City once!');
+//   }
+//   else{
+//     alert('Wrong! - I actually HAVE been to New York City.');
+//   }
+//
+//   var answer3 = prompt('Do I love jello?').toLowerCase();
+//   if(answer3 === 'yes' || answer3 === 'y') {
+//     alert('Wrong!!! No way, I can\'t stand jello.');
+//   }
+//   else{
+//     alert('Correct! Jello is NOT real food, and I DO NOT like it.');
+//   }
+//
+//   var answer4 = prompt('Have I ever parachuted before?').toLowerCase();
+//   if(answer4 === 'yes' || answer4 === 'y'){
+//     alert('Wrong! I would never jump out of an airplane.');
+//   }
+//   else{
+//     alert('Right! I play it safe everyday.');
+//   }
+//
+//   var answer5 = prompt('Do I enjoy pancakes?').toLowerCase();
+//   if(answer5 === 'yes' || answer5 === 'y') {
+//     alert('Of Course! Doesn\'t everyone love pancakes!');
+//   }
+//   else{
+//     alert('You are wrong! Pancakes are the best.');
+//   }
 
-  var answer2 = prompt('Have I been to New York City?').toLowerCase();
-  if(answer2 === 'yes' || answer2 === 'y') {
-    alert('Correct! I visted New York City once!');
-  }
-  else{
-    alert('Wrong! - I actually HAVE been to New York City.');
-  }
+// Question 6 guessing number game
+  // var correctNumber = 2;
+  // var i = 1;
+  // var guess1 = 5;
+  // var tryLeft = 4;
+  // var guess1 = (prompt('You have 4 attempts to guess an number between 1 and 10 \n \n GO FOR IT!'));
+  // var guess1Int = parseInt(guess1);
+  // console.log(guess1);
+  // while(i < 4){
+  //   if(guess1Int < 1 || guess1Int > 10){
+  //     alert('Outside of the range: \n \nPlease enter a number between 1 and 10.');
+  //     guess1 = (prompt('Please enter a number between 1 and 10'));
+  //     guess1Int = parseInt(guess1);
+  //   }
+  //   else if( Number.isInteger(parseFloat(guess1)) !== true ){
+  //     alert('Please enter an interger.');
+  //     guess1 = (prompt('Please enter a number between 1 and 10'));
+  //     guess1Int = parseInt(guess1);
+  //   }
+  //   else if( guess1Int < correctNumber){
+  //     alert('Sorry, that number is too LOW.');
+  //     tryLeft = 4 - i;
+  //     guess1 = (prompt('Please another number between 1 and 10. \n \n You have ' + tryLeft + ' tries left.'));
+  //     guess1Int = parseInt(guess1);
+  //     i = i + 1;
+  //   }
+  //   else if( guess1Int > correctNumber){
+  //     alert('Sorry, that number is too HIGH.');
+  //     tryLeft = 4 - i;
+  //     guess1 = prompt('Please another number between 1 and 10. \n \n You have ' + tryLeft + ' tries left.' );
+  //     guess1Int = parseInt(guess1);
+  //     i = i + 1;
+  //   }
+  //   else if (guess1Int === correctNumber){
+  //     alert('That is so awesomwe! You got it RIGHT!');
+  //     i = 5;
+  //   }
+  // }
+  // console.log('after while loop');
 
-  var answer3 = prompt('Do I love jello?').toLowerCase();
-  if(answer3 === 'yes' || answer3 === 'y') {
-    alert('Wrong!!! No way, I can\'t stand jello.');
-  }
-  else{
-    alert('Correct! Jello is NOT real food, and I DO NOT like it.');
-  }
 
-  var answer4 = prompt('Have I ever parachuted before?').toLowerCase();
-  if(answer4 === 'yes' || answer4 === 'y'){
-    alert('Wrong! I would never jump out of an airplane.');
-  }
-  else{
-    alert('Right! I play it safe everyday.');
-  }
-
-  var answer5 = prompt('Do I enjoy pancakes?').toLowerCase();
-  if(answer5 === 'yes' || answer5 === 'y') {
-    alert('Of Course! Doesn\'t everyone love pancakes!');
-  }
-  else{
-    alert('You are wrong! Pancakes are the best.');
-  }
 }
