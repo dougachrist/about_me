fbc826cb9f7a150d56e0f37d0fde70489d068eba
dougachrist <dougachrist@gmail.com>
2016-06-14 22:53:33 -0700

    clean up code and added comments

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 'use strict';
 
+// using a function to start the process when the button is clicked
 function startGame() {
 
+// Greeting the user
   var userName = prompt('Greetings!!! What is your name?');
   alert('Welcome ' + userName + ', glad you\'re here! \nI have a short quiz about me for you to complete.');
 
+//Questestions 1 - 5
   var answer1 = prompt('Do I have a dog?').toLowerCase();
   if(answer1 === 'yes' || answer1 === 'y') {
     alert('Right! Her name is Lucy.');
@@ -23,7 +26,7 @@ function startGame() {
 
   var answer3 = prompt('Do I love jello?').toLowerCase();
   if(answer3 === 'yes' || answer3 === 'y') {
-    alert('Wong!!! No way, I can\'t stand jello.');
+    alert('Wrong!!! No way, I can\'t stand jello.');
   }
   else{
     alert('Correct! Jello is NOT real food, and I DO NOT like it.');
