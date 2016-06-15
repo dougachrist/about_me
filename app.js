3cd5a7b91b5d1a86288b0667546dc3b021662837
dougachrist <dougachrist@gmail.com>
2016-06-14 21:42:56 -0700

    update validation rules

--- app.js
+++ app.js
@@ -3,43 +3,43 @@
 function startGame() {
 
   var userName = prompt('Greetings!!! What is your name?');
-  alert('Welcome ' + userName + ' glad you\'re here! \nI have a short quiz about me for you to complete');
+  alert('Welcome ' + userName + ', glad you\'re here! \nI have a short quiz about me for you to complete.');
 
   var answer1 = prompt('Do I have a dog?').toLowerCase();
-  if (answer1 === 'yes') {
-    alert('Right! Her name is Lucy');
+  if (answer1 === 'yes' || answer1 === 'y') {
+    alert('Right! Her name is Lucy.');
   }
   else{
-    alert('WRONG - I actually do have a dog');
+    alert('WRONG - I actually do have a dog.');
   }
 
-  var answer2 = prompt('Have I been to New York City?');
-  if(answer2 === 'yes') {
+  var answer2 = prompt('Have I been to New York City?').toLowerCase();
+  if(answer2 === 'yes'|| answer2 === 'y') {
     alert('Correct! I visted New York City once!');
   }
   else{
-    alert('Wrong! - I actually have been to New York City');
+    alert('Wrong! - I actually HAVE been to New York City.');
   }
 
-  var answer3 = prompt('Do I love jello?');
-  if(answer3 === 'yes') {
-    alert('Wong!!! No way, I can\'t stand jello');
+  var answer3 = prompt('Do I love jello?').toLowerCase();
+  if(answer3 === 'yes'|| answer3 === 'y') {
+    alert('Wong!!! No way, I can\'t stand jello.');
   }
   else{
-    alert('Correct! Jello is not real food, and I do not like it.');
+    alert('Correct! Jello is NOT real food, and I DO NOT like it.');
   }
 
-  var answer4 = prompt('Have I ever parachuted before?');
-  if(answer4 === 'yes'){
-    alert('Wrong! I would never jump out of an airplane');
+  var answer4 = prompt('Have I ever parachuted before?').toLowerCase();
+  if(answer4 === 'yes'|| answer4 === 'y'){
+    alert('Wrong! I would never jump out of an airplane.');
   }
   else{
     alert('Right! I play it safe everyday.');
   }
 
-  var answer5 = prompt('Do I enjoy pancakes?');
-  if(answer5 === 'yes') {
-    alert('Of Course! Doesn\'t everyone like pancakes!');
+  var answer5 = prompt('Do I enjoy pancakes?').toLowerCase();
+  if(answer5 === 'yes'|| answer5 === 'y') {
+    alert('Of Course! Doesn\'t everyone love pancakes!');
   }
   else{
     alert('You are wrong! Pancakes are the best.');
