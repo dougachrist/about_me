ca046a003f644e9d4649356625c77840e6837103
dougachrist <dougachrist@gmail.com>
2016-06-15 22:37:12 -0700

    code clean up

--- app.js
+++ app.js
@@ -4,49 +4,49 @@
 function startGame() {
 
 // Greeting the user
-  // var userName = prompt('Greetings!!! What is your name?');
-  // alert('Welcome ' + userName + ', glad you\'re here! \nI have a short quiz about me for you to complete.');
-  // var correctTally = 0;
+  var userName = prompt('Greetings!!! What is your name?');
+  alert('Welcome ' + userName + ', glad you\'re here! \nI have a short quiz about me for you to complete.');
+  var correctTally = 0;
 
 // Strech Goal - creating arrays for Questions, Answers, & Responses
-  // var questionsArray = [
-  //   'Do I have a dog?',
-  //   'Have I been to New York City?',
-  //   'Do I love jello?',
-  //   'Have I ever parachuted before?',
-  //   'Do I enjoy pancakes?'];
-  //
-  // var answersArray = ['yes','yes','no','no','yes'];
-  //
-  // var responsesTrueArray = [
-  //   'Right! Her name is Lucy.',
-  //   'Correct! I visted New York City once!',
-  //   'Correct! Jello is NOT real food, and I DO NOT like it.',
-  //   'Right! I play it safe everyday.',
-  //   'Of Course! Doesn\'t everyone love pancakes!'];
-  //
-  // var responsesFalseArray = [
-  //   'WRONG - I actually do have a dog.',
-  //   'Wrong! - I actually HAVE been to New York City.',
-  //   'Wrong!!! No way, I can\'t stand jello.',
-  //   'Wrong! I would never jump out of an airplane.',
-  //   'You are wrong! Pancakes are the best.'];
-  //
-  // var responsesArray = [responsesTrueArray, responsesFalseArray];
-  // var hugeMultiArray = [questionsArray, answersArray, responsesArray];
+  var questionsArray = [
+    'Do I have a dog?',
+    'Have I been to New York City?',
+    'Do I love jello?',
+    'Have I ever parachuted before?',
+    'Do I enjoy pancakes?'];
+
+  var answersArray = ['yes','yes','no','no','yes'];
+
+  var responsesTrueArray = [
+    'Right! Her name is Lucy.',
+    'Correct! I visted New York City once!',
+    'Correct! Jello is NOT real food, and I DO NOT like it.',
+    'Right! I play it safe everyday.',
+    'Of Course! Doesn\'t everyone love pancakes!'];
+
+  var responsesFalseArray = [
+    'WRONG - I actually do have a dog.',
+    'Wrong! - I actually HAVE been to New York City.',
+    'Wrong!!! No way, I can\'t stand jello.',
+    'Wrong! I would never jump out of an airplane.',
+    'You are wrong! Pancakes are the best.'];
+
+  var responsesArray = [responsesTrueArray, responsesFalseArray];
+  var hugeMultiArray = [questionsArray, answersArray, responsesArray];
 
 //Stech Goal - code to compare responses with the answers and give result
-  // for( var i = 0; i < questionsArray.length; i++){
-  //   var answerA = prompt(questionsArray[i]).toLowerCase();
-  //   var answerAFirst = answerA.substring(0,1);
-  //   if(answerA === answersArray[i] || answerAFirst === answersArray[i].substring(0,1)){
-  //     alert(responsesArray[0][i]);
-  //     correctTally++;
-  //   }
-  //   else {
-  //     alert(responsesArray[1][i]);
-  //   }
-  // }
+  for( var i = 0; i < questionsArray.length; i++){
+    var answerA = prompt(questionsArray[i]).toLowerCase();
+    var answerAFirst = answerA.substring(0,1);
+    if(answerA === answersArray[i] || answerAFirst === answersArray[i].substring(0,1)){
+      alert(responsesArray[0][i]);
+      correctTally++;
+    }
+    else {
+      alert(responsesArray[1][i]);
+    }
+  }
 
 // //Questestions 1 - 5
   // var answer1 = prompt('Do I have a dog?').toLowerCase();
@@ -139,42 +139,42 @@ function startGame() {
 
   // Question 7 guess Beers
 
-  // var myBeers = ['Firestone','Fat Tire','Blue Moon'];
-  // var myBeersLower = ['firestone', 'fat tire', 'blue moon'];
-  // var myBeersSpace = ['Firestone',' Fat Tire',' Blue Moon'];
-  // var response = prompt('You have 6 attempts to guess my 3 favorite beers \n \n GO FOR IT!').toLowerCase();
-  // var counter = 1;
-  // var favBeer = 0;
-  // var trysLeft = 5;
-  // for( var counter = 1 ; counter <= 6; counter++){
-  //   for( var i = 0; i < myBeers.length; i++ ){
-  //     if(response === myBeersLower[i] ){
-  //       var favBeerNumb = i;
-  //       var rightBeer = 1;
-  //     }
-  //     else{
-  //       favBeer = 0;
-  //     }
-  //   }
-  //   if(rightBeer === 1){
-  //     alert('Correct: ' + myBeers[favBeerNumb] + ' is one of my favorite beers. \n \n My favorite beers are: ' + myBeersSpace);
-  //     counter = 7;
-  //     correctTally++;
-  //   }
-  //   else if(favBeer === 0 && counter < 6){
-  //     if(counter < 5){ // need a simple if stmt to change between "tries" and "try"
-  //       var tries = 'tries';
-  //     }
-  //     else{
-  //       var tries = 'try';
-  //     }
-  //     response = prompt('Sorry that was NOT one of my favorite beers.\n\n you have: ' + trysLeft + ' ' + tries + ' left').toLowerCase();
-  //   }
-  //   else if(favBeer === 0 && counter >= 6){
-  //     alert('Sorry, that was NOT one of my favorite beers.\n\n My favorite beers are ACTUALLY\n\n' + myBeersSpace );
-  //   }
-  //   trysLeft = trysLeft - 1;
-  // }
+  var myBeers = ['Firestone','Fat Tire','Blue Moon'];
+  var myBeersLower = ['firestone', 'fat tire', 'blue moon'];
+  var myBeersSpace = ['Firestone',' Fat Tire',' Blue Moon'];
+  var response = prompt('You have 6 attempts to guess my 3 favorite beers \n \n GO FOR IT!').toLowerCase();
+  var counter = 1;
+  var favBeer = 0;
+  var trysLeft = 5;
+  for( var counter = 1 ; counter <= 6; counter++){
+    for( var i = 0; i < myBeers.length; i++ ){
+      if(response === myBeersLower[i] ){
+        var favBeerNumb = i;
+        var rightBeer = 1;
+      }
+      else{
+        favBeer = 0;
+      }
+    }
+    if(rightBeer === 1){
+      alert('Correct: ' + myBeers[favBeerNumb] + ' is one of my favorite beers. \n \n My favorite beers are: ' + myBeersSpace);
+      counter = 7;
+      correctTally++;
+    }
+    else if(favBeer === 0 && counter < 6){
+      if(counter < 5){ // need a simple if stmt to change between "tries" and "try"
+        var tries = 'tries';
+      }
+      else{
+        var tries = 'try';
+      }
+      response = prompt('Sorry that was NOT one of my favorite beers.\n\n you have: ' + trysLeft + ' ' + tries + ' left').toLowerCase();
+    }
+    else if(favBeer === 0 && counter >= 6){
+      alert('Sorry, that was NOT one of my favorite beers.\n\n My favorite beers are ACTUALLY\n\n' + myBeersSpace );
+    }
+    trysLeft = trysLeft - 1;
+  }
 
 // CorrectTally counter
 
